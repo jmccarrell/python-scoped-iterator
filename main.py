18f182273a738a1d3dd563a73e0eef8b7bee04fc
Jeff McCarrell <jeff@mccarrell.org>
2018-07-06 13:13:56 -0700

    initial commit of scoped iterator.  simple test passes.

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import io
 import pytest
+import more_itertools
 
 
 def vals():
-    for a in (['a'] * 3):
+    for a in (['CA'] * 3):
         yield a
-    for b in (['b'] * 2):
+    for b in (['FL'] * 2):
         yield b
+
+def expected_vals_with_markers():
+    expected = list()
+    expected.append(('start_CA'))
+    for c in (['CA'] * 3):
+        expected.append(c)
+    expected.append(('stop_CA'))
+    expected.append(('start_FL'))
+    for c in (['FL'] * 2):
+        expected.append(c)
+    expected.append(('stop_FL'))
+    return expected
 
 def expected_vals():
     expected = list()
-    expected.append(('start_a'))
-    for c in (['a'] * 3):
+    for c in (['CA'] * 3):
         expected.append(c)
-    expected.append(('stop_a'))
-    expected.append(('start_b'))
-    for c in (['b'] * 2):
+    for c in (['FL'] * 2):
         expected.append(c)
-    expected.append(('stop_b'))
     return expected
 
-def test_scoped_iteration():
+def test_fixtures():
     assert list(vals()) == expected_vals()
 
 class context_marker():
@@ -29,11 +38,11 @@ class context_marker():
         self.out = out
 
     def __enter__(self):
-        print(f"start_{self.val}", file=self.out)
+        print(f'start_{self.val}', file=self.out)
         return self
 
     def __exit__(self, execption, value, traceback):
-        print(f"stop_{self.val}", file=self.out)
+        print(f'stop_{self.val}', file=self.out)
         return False
 
 def test_context_marker():
@@ -42,7 +51,56 @@ def test_context_marker():
         with context_marker('z', buf) as b:
             print('working', file=buf)
 
-        
         buf.seek(0)
         s = buf.read().rstrip()
         assert s.split('\n') == expected
+
+class scoped_iterator():
+    def __init__(self, iterator, dispatch_table):
+        self.iter = more_itertools.peekable(iterator)
+        self.scope = None
+        self.scope_dispatch = dispatch_table
+        self.sentinel = '__sentinel__'
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        i = self.iter.peek(self.sentinel)
+        if i == self.sentinel:
+            # end of iteration stream
+            if self.scope is not None:
+                self.scope.__exit__(None, None, None)
+                self.scope = self.scope_key = None
+        elif self.scope is None:
+            # start of new scope
+            if i in self.scope_dispatch:
+                self.scope_key = i
+                self.scope = self.scope_dispatch[self.scope_key](self.scope_key)
+                self.scope.__enter__()
+        elif i != self.scope_key and i in self.scope_dispatch:
+            # change in scope
+            self.scope.__exit__(None, None, None)
+            self.scope_key = i
+            self.scope = self.scope_dispatch[self.scope_key](self.scope_key)
+            self.scope.__enter__()
+
+        return next(self.iter)
+
+def test_normal_marker():
+    with io.StringIO() as buf:
+        def ca_scope_factory(marker):
+            return context_marker(marker, buf)
+        def fl_scope_factory(marker):
+            return context_marker(marker, buf)
+
+        dispatch_table = { 'CA': ca_scope_factory,
+                           'FL': fl_scope_factory }
+
+        it = scoped_iterator(vals(), dispatch_table)
+        for i in it:
+            print(i, file=buf)
+
+        buf.seek(0)
+        s = buf.read().rstrip()
+        assert s.split('\n') == expected_vals_with_markers()
